0db700498a16d5b98643a953d6e1756043e67edf
Nikki Apostolakis <nikki.apostolakis@gmail.com>
2018-03-15 15:19:52 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,11 +1,8 @@
 def square_array(array)
   # your code here
-  # sqdArray=[]
-  # array.each do |num|
-  #   sqdArray << num*num
-  # end
-  # return sqdArray
-
-  array.collect!{|x| x*x}
-
+  sqdArray=[]
+  array.each do |num|
+    sqdArray << num*num
+  end
+  return sqdArray
 end
